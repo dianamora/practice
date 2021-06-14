9ae81f41466d291a5675b37b382a346aeb712678
Diana Mora <morra.dianna@gmail.com>
2021-06-14 12:17:10 -0400

    added bootstrap to application.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  #GET /about
   get "about" => "about#index"
 
-  get "/" => "main#index"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "main#index"
+ 
 end
+
+ # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
