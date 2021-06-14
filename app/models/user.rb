7f834603aa07af013a2b7a04f1375a3730e5131b
Diana Mora <morra.dianna@gmail.com>
2021-06-14 14:40:30 -0400

    added sign up page/form and log out button

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
+    has_many :twitter_accounts
     has_secure_password
+    validates :email, presence: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: "must be a valid email address"}
+    validates :password, presence: true
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
   get "about" => "about#index"
+  
+  get "sign_up" => "registrations#new"
+  post "sign_up" => "registrations#create"
+
+  get "sign_in" => "sessions#new"
+  post "sign_in" => "sessions#create"
+
+  delete "logout" => "sessions#destroy"
 
   root to: "main#index"
  
